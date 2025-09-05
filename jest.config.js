9ba1f544fa5c38ab408c6d1677f4ced3b6fdedfa
nhat28-10 <nhatnguyen281003@gmail.com>
2025-09-05 12:16:09 +0700

    Setup Jest for Project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable prettier/prettier */
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 type Handle = () => Promise<string>
 const fullname: string = 'Nguyễn Thanh Minh Nhật'
 const person: { name: string } = { name: fullname }
@@ -6,3 +7,7 @@ const handle: Handle = () => Promise.resolve(fullname)
 // console.log(name)
 
 handle().then(console.log)
+
+export function sum(a: number, b: number): number {
+  return a + b
+}
